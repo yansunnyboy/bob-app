839f5b9c352c76043058b30c7ae508c4b9fd5e21
Yan Sun <ysun971@gmail.com>
2021-11-30 17:41:26 +1100

    update with data scraper completed in seeds file

--- app/controllers/scraper.rb
+++ app/controllers/scraper.rb
@@ -1,27 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 require 'open-uri'
 require 'nokogiri'
 require 'byebug'
 
-
-
-
-
-
-
-
-
-
-
-
-
-categories.each do |category|
+def scraper(category)
   html_file = URI.open("https://www.producthunt.com/search?q=#{category}").read
   html_doc = Nokogiri::HTML(html_file)
   html_doc.search(".styles_item__2kQQ5").each do |product|
@@ -32,10 +13,7 @@ categories.each do |category|
     path = product_doc.search(".styles_headerInfo__3h0jF h1 a").attribute('href').value
     begin
       product_site = URI.open("https://www.producthunt.com#{path}")
-      _product_html = Nokogiri::HTML(product_site)
-    rescue OpenURI::HTTPError => _e
-      puts "HTTP error occured"
-      next
+      product_html = Nokogiri::HTML(product_site)
     rescue RuntimeError => e
       if />.*[?]/.match(e.message)
         url = />.*[?]/.match(e.message).to_s.chars[2...-1].join
@@ -44,6 +22,6 @@ categories.each do |category|
       end
     end
     url = "https://www.producthunt.com#{path}" if url.nil?
-    Product.create(name: name, url: url)
+    Product.new(name: name, url: url)
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ApplicationRecord
   validates :name, presence: true, uniqueness: true, allow_blank: false
   validates :url, uniqueness: true, presence: true, allow_blank: false
-  validates :url, format: URI::regexp(%w[https http])
+  validates :url, format: URI::DEFAULT_PARSER.make_regexp(%w[https http])
 end
