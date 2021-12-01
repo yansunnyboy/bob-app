75d4ac99623a86cb865b027f8bfced239fdec703
Emmanuel Cadot <emacadot@gmail.com>
2021-12-01 15:17:23 +1100

    user journey

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
-  # before_action :authenticate_user!
+  before_action :authenticate_user!
+
+  def after_sign_in_path_for(user)
+   # stored_location_for(user) || welcome_path
+    products_path
+  end
+
 end
