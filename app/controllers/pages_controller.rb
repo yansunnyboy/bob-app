4b617217f5d8bcc68d52efe0a394bde20d86b909
Sarah Alexander <sjalex78@gmail.com>
2021-12-06 21:41:11 +1100

    front page category tags added to landing page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,10 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
+    @products = Product.all
+    @categories = ActsAsTaggableOn::Tag.all
+  end
+
+  def index
   end
 end
