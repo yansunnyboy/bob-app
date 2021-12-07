896634decf94118219de9d7f3f2a8af89a31a47e
Sarah Alexander <sjalex78@gmail.com>
2021-12-07 19:18:04 +1100

    commented out broken tailwind color config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,19 +2,19 @@ module.exports = {
   purge: ["./app/**/*.html.erb"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    colors: {
-      purple: {
-        light: '#8b80fe',
-        DEFAULT: '#6c60ff',
-        dark: '#271ca2',
-      },
-      pink: {
-        DEFAULT: '#fec2f2',
-      },
-      white: {
-        DEFAULT: '#fffdff',
-      },
-    },
+    // colors: {
+    //   purple: {
+    //     light: '#8b80fe',
+    //     DEFAULT: '#6c60ff',
+    //     dark: '#271ca2',
+    //   },
+    //   pink: {
+    //     DEFAULT: '#fec2f2',
+    //   },
+    //   white: {
+    //     DEFAULT: '#fffdff',
+    //   },
+    // },
   },
   variants: {
     extend: {},
