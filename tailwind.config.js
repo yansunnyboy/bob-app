0dbb8bb571189b491afb21ad34f7f3e61dbfbab8
Yan Sun <67826503+yansunnyboy@users.noreply.github.com>
2021-12-07 16:57:26 +1100

    Revert "update with changes to color scheme and layout"

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,19 +2,7 @@ module.exports = {
   purge: ["./app/**/*.html.erb"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    colors: {
-      purple: {
-        light: '#8b80fe',
-        DEFAULT: '#6c60ff',
-        dark: '#271ca2',
-      },
-      pink: {
-        DEFAULT: '#fec2f2',
-      },
-      white: {
-        DEFAULT: '#fffdff',
-      },
-    },
+    extend: {},
   },
   variants: {
     extend: {},
